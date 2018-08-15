6aa9656f891a495d38bfd0eaf50b92fb5db138f4
Andrew Brown <andrew.brown2@pega.com>
2018-08-15 10:15:20 -0400

    Initial Commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const fetch = require('node-fetch');
 const Snoowrap = require('snoowrap');
 const Snoostorm = require('snoostorm');
 
@@ -15,8 +16,8 @@ const client = new Snoostorm(r);
 
 // Configure options for stream: subreddit & results per query
 const streamOpts = {
-    subreddit: 'all',
-    results: 25
+    subreddit: 'ffdevelopers',
+    results: 10
 };
 
 // Create a Snoostorm CommentStream with the specified options
@@ -24,7 +25,32 @@ const comments = client.CommentStream(streamOpts); // eslint-disable-line
 
 // On comment, perform whatever logic you want to do
 comments.on('comment', (comment) => {
-    if (comment.body === ':(') {
-        comment.reply(':)');
+    if (comment.body.startsWith('!patsbot')) {
+        var replytext = '';
+        // Format arguments: remove '!patsbot' and change spaces to +
+        var gif_args = comment.body.replace('!patsbot ', '');
+        var gif_api = 'https://patriotsdynasty.info/reddit/patsbot/' + (gif_args.replace(/\s/g, '+'));
+        console.log(gif_api);
+        // Get the data and return it as a comment reply.
+        fetch(gif_api)
+            .then((resp) => resp.json())
+            .then(function(json) {
+                json.forEach(function(obj) { 
+                    replytext += "* [" + obj.title + " (" + obj.season + " Week " + obj.week + ")](https://gfycat.com/" + obj.gfycat + ") \n"; 
+                });
+            })
+            .then(function(){
+                console.log(replytext);
+                if (replytext) {
+                    comment.reply(replytext);    
+                }
+                else {
+                    comment.reply("Sorry, there's no highlights for those terms.");
+                    console.log("Sorry, there's no highlights for those terms.");
+                }
+            })
+            .catch(function(error) {
+                replytext += 'Whoops, something went wrong!';
+            });
     }
 });
