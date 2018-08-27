91ee2208603792bbb44cf19550c6b4be283b9948
Andrew Brown <andrew.brown2@pega.com>
2018-08-27 12:03:34 -0400

    Adding testing subreddit.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const client = new Snoostorm(r);
 
 // Configure options for stream: subreddit & results per query
 const streamOpts = {
-    subreddit: 'dyj+patriots',
+    subreddit: 'dyj+patriots+ffdevelopers',
     results: 25
 };
 
