86a2264cd5a0a73549fc40ba0bc084f766e6d298
Andrew Brown <arbrown83@gmail.com>
2021-08-31 08:51:09 -0400

    Converting the app to manage Reddit Game Threads instead.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 
-const fetch = require('node-fetch');
 const Snoowrap = require('snoowrap');
 const Snoostorm = require('snoostorm');
+
+// Create a place to store the content.
+const store = require('data-store')({ path: process.cwd() + '/gifdata.json' });
 
 // Build Snoowrap and Snoostorm clients
 const r = new Snoowrap({
@@ -16,114 +18,29 @@ const client = new Snoostorm(r);
 
 // Configure options for stream: subreddit & results per query
 const nflStreamOpts = {
-    subreddit: 'nfl+nflgifbot+dyj+patriots',
+    subreddit: 'nflgifbot+dyj',
     results: 40
 };
 
 // Create a Snoostorm CommentStream with the specified options
 const comments = client.CommentStream(nflStreamOpts); // eslint-disable-line
 
-// On comment, respond with NFL Gifs.
+// Look for Timnog gif link comments.
 comments.on('comment', (comment) => {
-	if (comment.body.startsWith('!patsbot')) {
-        var replytext = '';
-        // Format arguments: remove '!patsbot' and change spaces to +
-        var gif_args = comment.body.replace('!patsbot ', '');
-        gif_args.replace('\\_', '_');
-        console.log(gif_args);
-        var special_args = {
-            patsbot: 'Hey, stop trying to break me, I have feelings too!',
-            belichick: "1. [Curious Belichick](https://gfycat.com/ComfortableSmoggyHornedtoad) \n1. [Exasperated Belichick](https://gfycat.com/WillingBlaringAmericancicada)\n1. [Confused Belichick](https://gfycat.com/ImprobableFormalGerenuk) \n1. [Scoffing Belichick](https://gfycat.com/gentlecreamydungbeetle) \n1. [Angry Belichick](https://gfycat.com/LikableFortunateKiskadee)",
-            buttfumble: "1. [The Butt Fumble](https://gfycat.com/InsidiousDetailedHermitcrab) \n1. [Butt Fumble Replay](https://gfycat.com/PortlySorrowfulAllensbigearedbat)",
-        };
-        if (gif_args in special_args) {
-            var custom_message = special_args[gif_args];
-            comment.reply(custom_message);
-            console.log('Custom Response sent: ' + custom_message);
+    // Get comments from gif posters that have a link in 'em.
+	if ((comment.author.name == 'arbrown83' || comment.author.name == 'timnog') && comment.body_html.includes('href')) {
+        // If the link is from a gif site, save it.
+        if (comment.body_html.includes('gfycat') || comment.body_html.includes('streamable')) {
+            console.log(comment.body);
+            var today = new Date();
+            var dd = String(today.getDate()).padStart(2, '0');
+            var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+            var yyyy = today.getFullYear();
+            today = yyyy + mm + dd;
+            // Save to JSON file.
+            store.union(today, comment.body);
+            console.log(store)
         }
-        else {
-            var gif_api = 'https://patriotsdynasty.info/reddit/patsbot/' + (gif_args.replace(/\s/g, '+'));
-            console.log(gif_api);
-        }
-        // Get the data and return it as a comment reply.
-        fetch(gif_api)
-            .then((resp) => resp.json())
-            .then(function(json) {
-                json.forEach(function(obj) { 
-                    console.log(obj.streamable);
-                    if(obj.streamable == "True") {
-                        replytext += "1. [" + obj.title + " (" + obj.season + " Week " + obj.week + ")](https://streamable.com/" + obj.gfycat + ") \n";
-                    }
-                    else {
-                        replytext += "1. [" + obj.title + " (" + obj.season + " Week " + obj.week + ")](https://gfycat.com/" + obj.gfycat + ") \n";
-                    }
-                    
-                });
-
-            })
-            .then(function(){
-                
-                if (replytext) {
-                    replytext += "\nI'm a bot! Want to learn more about me? [Click here!](https://patriotsdynasty.info/patsbot-instructions)\n";
-                    replytext += "\nGot a suggestion for the bot or a missing highlight? [Let me know.](https://patriotsdynasty.info/contact/feedback)";
-                    comment.reply(replytext);   
-                    console.log('Reply Text: ' + replytext); 
-                }
-                
-                else {
-                    comment.reply("Sorry, there's no highlights for those terms.\n I'm a bot! Want to learn more about me? [Click here!](https://patriotsdynasty.info/patsbot-instructions)\nGot a suggestion for the bot or a missing highlight? [Let me know.](https://patriotsdynasty.info/contact/feedback)");
-                    console.log("Sorry, there's no highlights for those terms.");
-                }
-            })
-            .catch(function(error) {
-                replytext += 'Whoops, something went wrong!';
-            });
-    }
-    if (comment.body.startsWith('!gifbot')) {
-        var replytext = '';
-        // Format arguments: remove '!gifbot' and change spaces to +
-        var gif_args = comment.body.replace('!gifbot ', '');
-        gif_args.replace('\\_', '_');
-        console.log(gif_args);
-        var special_args = {
-            gifbot: 'Hey, stop trying to break me, I have feelings too!',
-            hernandez: 'C\'mon now. Nobody needs that.',
-        };
-        if (gif_args in special_args) {
-            var custom_message = special_args[gif_args];
-            comment.reply(custom_message);
-            console.log('Custom Response sent: ' + custom_message);
-        }
-        else {
-            var gif_api = 'http://dev-nfl-gifbot.pantheonsite.io/gifbot?search=' + (gif_args.replace(/\s/g, '+'));
-            console.log(gif_api);
-        }
-        // Get the data and return it as a comment reply.
-        fetch(gif_api)
-            .then((resp) => resp.json())
-            .then(function(json) {
-                json.forEach(function(obj) { 
-                    replytext += "1. [" + obj.title + " (" + obj.season + ")](https://gfycat.com/" + obj.gfycat + ") \n";
-                });
-
-            })
-            .then(function(){
-                
-                if (replytext) {
-                    replytext += "\nI'm a bot! Want to learn more about me? [Click here!](https://github.com/oooShiny/nflgifbot)\n";
-                    replytext += "\nGot a suggestion for the bot or a missing highlight? [Let me know.](https://www.reddit.com/message/compose/?to=nflgifbot)";
-                    comment.reply(replytext);   
-                    console.log('Reply Text: ' + replytext); 
-                }
-                
-                else {
-                    comment.reply("Sorry, I couldn't find any highlights for those terms.\n I'm a bot! Want to learn more about me? [Click here!](https://github.com/oooShiny/nflgifbot)\nGot a suggestion for the bot or a missing highlight? [Let me know.](https://www.reddit.com/message/compose/?to=nflgifbot)");
-                    console.log("Sorry, there's no highlights for those terms.");
-                }
-            })
-            .catch(function(error) {
-                replytext += 'Whoops, something went wrong!';
-            });
     }
 });
 
@@ -139,7 +56,6 @@ if (post.link_flair_text == 'Highlights' || post.title.startsWith('[Highlight]')
     else {
         var title = post.title;
     }
-    var post_gif = 'http://dev-nfl-gifbot.pantheonsite.io/bot/convert?title=' + title + '&link=' + post.url;
     console.log(post_gif);
     fetch(post_gif);
 }
